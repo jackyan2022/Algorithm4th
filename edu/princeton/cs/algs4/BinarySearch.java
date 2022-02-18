58a71c9e9064c6be29f4323ac9021d96fd313126
yan zhong <jackyan63@gmail.com>
2022-02-17 19:49:41 -0500

    binary search

--- edu/princeton/cs/algs4/BinarySearch.java
+++ edu/princeton/cs/algs4/BinarySearch.java
@@ -56,11 +56,10 @@ public class BinarySearch {
     public static int indexOf(int[] a, int key) {
         int lo = 0;
         int hi = a.length - 1;
-        while (lo <= hi) {
-            // Key is in a[lo..hi] or not present.
+        while(lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-            if      (key < a[mid]) hi = mid - 1;
-            else if (key > a[mid]) lo = mid + 1;
+            if     (key < a[mid]) hi = mid - 1;
+            else if(key > a[mid]) lo = mid + 1;
             else return mid;
         }
         return -1;
